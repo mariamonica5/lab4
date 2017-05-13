e86995f79e3bc7bdc049807b8afb1eeccb44b20d
Moni <monko95@yahoo.com>
2017-05-13 21:23:09 +0300

    4

--- src/test/java/model/StudentTest.java
+++ src/test/java/model/StudentTest.java
@@ -23,57 +23,41 @@ public class StudentTest {
     @Test
     public void getRegNumber() throws Exception {
 
-        Student stud1= new Student("fghs5679", "Pop", "Gigel", 342);
-        Student stud2= new Student("abcd1234", "Pop", "Viorica", 212);
-        assertEquals("fghs5679", stud1.getRegNumber());
-        assertEquals("abcd1234", stud2.getRegNumber());
-
     }
 
     @Test
     public void setRegNumber() throws Exception {
 
-        Student stud1= new Student("fghs5679", "Pop", "Gigel", 342);
-        stud1.setRegNumber("wers1234");
-        assertEquals("wers1234", stud1.getRegNumber());
+    }
+
+    @Test
+    public void getSurname() throws Exception {
 
     }
 
+    @Test
+    public void setSurname() throws Exception {
+
+    }
 
     @Test
     public void getName() throws Exception {
 
-        Student stud1= new Student("fghs5679", "Pop", "Gigel", 342);
-        Student stud2= new Student("abcd1234", "Ionescu", "Viorica", 212);
-        assertEquals("Pop", stud1.getName());
-        assertEquals("Ionescu", stud2.getName());
     }
 
     @Test
     public void setName() throws Exception {
-
-        Student stud1= new Student("fghs5679", "Pop", "Gigel", 342);
-        stud1.setName("Popescu");
-        assertEquals("Popescu", stud1.getName());
 
     }
 
     @Test
     public void getGroup() throws Exception {
 
-        Student stud1= new Student("fghs5679", "Pop", "Gigel", 342);
-        Student stud2= new Student("abcd1234", "Ionescu", "Viorica", 212);
-        assertEquals(342, stud1.getGroup());
-        assertEquals(212, stud2.getGroup());
-
     }
 
     @Test
     public void setGroup() throws Exception {
 
-        Student stud1= new Student("fghs5679", "Pop", "Gigel", 342);
-        stud1.setGroup(18);
-        assertEquals(18, stud1.getGroup());
     }
 
 }
